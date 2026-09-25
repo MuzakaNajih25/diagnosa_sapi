ba4f8c27c38b3f559e80f491ff5a7eb630acda77
MuzakaNajih25 <muzakanajih058@gmail.com>
2026-09-25 12:53:37 +0700

    fix: adjust spacing in KonsultasiController

--- app/Http/Controllers/KonsultasiController.php
+++ app/Http/Controllers/KonsultasiController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\DB; 
 
 class KonsultasiController extends Controller 
 {
