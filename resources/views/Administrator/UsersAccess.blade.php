5d64ceddec1264dba688207abd990bc59f65ace5
MuzakaNajih25 <muzakanajih058@gmail.com>
2026-09-25 12:53:53 +0700

    fix: remove trailing whitespace in applySort function

--- resources/views/Administrator/UsersAccess.blade.php
+++ resources/views/Administrator/UsersAccess.blade.php
@@ -607,7 +607,7 @@ function applySort() {
         }
         va = String(va).toLowerCase();
         vb = String(vb).toLowerCase();  
-        return sortDir === 'asc' ? va.localeCompare(vb) : vb.localeCompare(va);
+        return sortDir === 'asc' ? va.localeCompare(vb) : vb.localeCompare(va); 
     });   
 }
 
